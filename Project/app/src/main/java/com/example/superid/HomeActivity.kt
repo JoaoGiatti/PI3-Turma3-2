e0536d3e2f9daf32094d414261b86837576ab0ae
João Giatti <joaogiatti2006@gmail.com>
2025-05-06 19:53:16 -0300

    feat: first NavBar structure (not working)

--- Project/app/src/main/java/com/example/superid/HomeActivity.kt
+++ Project/app/src/main/java/com/example/superid/HomeActivity.kt
@@ -3,23 +3,82 @@ package com.example.superid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.superid.ui.theme.SuperIDTheme
+import androidx.compose.ui.graphics.Color
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            SuperIDTheme {
+                HomeScreen()
+            }
         }
     }
 }
 
 @Composable
-fun HomeScreen() {
-    Text("Tela de home aaaaaaa")
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+    val navItemList = listOf(
+        NavItem("Senhas", R.drawable.keyicon),
+        NavItem("Escanear", R.drawable.scanicon),
+        NavItem("perfil", R.drawable.profileicon)
+    )
+
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar(
+                containerColor = Color(0xFF252525)
+            ) {
+                navItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon =  { Icon(
+                            painter = painterResource(id = navItem.icon),
+                            contentDescription = navItem.label,
+                            modifier = Modifier.size(30.dp)
+                        )},
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color(0xFFF4EB00),
+                            unselectedIconColor = Color.Gray,
+                            indicatorColor = Color.Transparent
+                        )
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        ContentScreen(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier){
+
 }
---
+++ Project/app/src/main/java/com/example/superid/NavItem.kt
@@ -0,0 +1,9 @@
+package com.example.superid
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+class NavItem(
+    val label: String,
+    val icon: Int,
+) {
+}
